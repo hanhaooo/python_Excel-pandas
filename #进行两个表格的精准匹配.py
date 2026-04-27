5cd0ceb1c1713f49c062b14776a5d49b7c6bc6d1
hanfucius <hanfucius@126.com>
2026-04-27 19:53:58 +0800

    进行部分代码编写

--- #进行两个表格的精准匹配.py
+++ #进行两个表格的精准匹配.py
@@ -1,2 +1,11 @@
 #进行两个表格的精准匹配
 import pandas as pd
+
+#创建独立的函数，处理两个excel的数据匹配工作，输出匹配上的文件和未匹配上的数据
+def match_tables(table_left,table_right,code)
+    try:
+        match_data = table_left.merge(table_right,on = code,how = 'left',indicator = True)
+        m_table_left = table_left[match_data['_merge'] == 'left_only'].copy()
+        m_table_right = table_right[match_data['_merge'] == 'right_only'].copy()
+        um_mark = match_data[]
+        
